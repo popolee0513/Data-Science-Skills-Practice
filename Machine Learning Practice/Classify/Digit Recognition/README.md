4af1e83a680023d01266275f456c61c9e824be0c
popolee0513 <b05801037@ntu.edu.tw>
2020-03-19 19:13:04 +0800

    Add files via upload

---
+++ Machine Learning Practice/Classify/Digit Recognition/README.md
@@ -0,0 +1,15 @@
+PASS THE TEST
+---------------
+![image](https://github.com/popolee0513/Machine-Learning-with-Python-From-Linear-Models-to-Deep-Learning/blob/master/Digit%20Recognition%20Part%201/PASS%20THE%20AUTOMATIC%20TEST.png)
+
+My Work
+-------------------------
+Impement
+
+1.Linear Regression with Closed Form Solution
+
+2.Multinomial (Softmax) Regression and Gradient Descent
+
+3.PCA and Kernel methods
+
+4.SVM (Just calling regular sciket-learn module)
---
+++ Machine Learning Practice/Classify/Digit Recognition/softmax.py
@@ -0,0 +1,212 @@
+import sys
+sys.path.append("..")
+import utils
+from utils import *
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.sparse as sparse
+
+
+def augment_feature_vector(X):
+    """
+    Adds the x[i][0] = 1 feature for each data point x[i].
+
+    Args:
+        X - a NumPy matrix of n data points, each with d - 1 features
+
+    Returns: X_augment, an (n, d) NumPy array with the added feature for each datapoint
+    """
+    column_of_ones = np.zeros([len(X), 1]) + 1
+    return np.hstack((column_of_ones, X))
+
+def compute_probabilities(X, theta, temp_parameter):
+    """
+    Computes, for each datapoint X[i], the probability that X[i] is labeled as j
+    for j = 0, 1, ..., k-1
+
+    Args:
+        X - (n, d) NumPy array (n datapoints each with d features)
+        theta - (k, d) NumPy array, where row j represents the parameters of our model for label j
+        temp_parameter - the temperature parameter of softmax function (scalar)
+    Returns:
+        H - (k, n) NumPy array, where each entry H[j][i] is the probability that X[i] is labeled as j
+        
+    """
+    H=np.zeros((theta.shape[0],X.shape[0]))
+    for i in range(X.shape[0]):
+        c=[]
+        for j in range(theta.shape[0]):
+            H[j,i]=np.dot(X[i],theta[j])/temp_parameter
+            c.append(np.dot(X[i],theta[j])/temp_parameter)
+        maxi=max(c)
+        H[:,i]=np.exp(H[:,i]-maxi)
+        total=np.sum(H[:,i])
+        H[:,i]=H[:,i]/total    
+    return H
+
+def compute_cost_function(X, Y, theta, lambda_factor, temp_parameter):
+    """
+    Computes the total cost over every datapoint.
+
+    Args:
+        X - (n, d) NumPy array (n datapoints each with d features)
+        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
+            data point
+        theta - (k, d) NumPy array, where row j represents the parameters of our
+                model for label j
+        lambda_factor - the regularization constant (scalar)
+        temp_parameter - the temperature parameter of softmax function (scalar)
+
+    Returns
+        c - the cost value (scalar)
+    """
+    #YOUR CODE HERE
+    lambd=sum(sum((lambda_factor/2)*(theta**2)))
+    temp=0
+    H=compute_probabilities(X, theta, temp_parameter)
+    b = np.zeros((Y.size, H.shape[0]))
+    b[np.arange(Y.size),Y] = 1
+    # b - (n,k)
+    # H - (k, n) NumPy array, where each entry H[j][i] is the probability that X[i] is labeled as j
+    # H.T -(n,k)
+    cost=-np.multiply(np.log(H.T),b)
+    cost=sum(sum(cost))/X.shape[0]
+    return lambd+cost
+
+def run_gradient_descent_iteration(X, Y, theta, alpha, lambda_factor, temp_parameter):
+    """
+    Runs one step of batch gradient descent
+
+    Args:
+        X - (n, d) NumPy array (n datapoints each with d features)
+        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
+            data point
+        theta - (k, d) NumPy array, where row j represents the parameters of our
+                model for label j
+        alpha - the learning rate (scalar)
+        lambda_factor - the regularization constant (scalar)
+        temp_parameter - the temperature parameter of softmax function (scalar)
+
+    Returns:
+        theta - (k, d) NumPy array that is the final value of parameters theta
+    """
+    #YOUR CODE HERE
+    H=compute_probabilities(X, theta, temp_parameter)
+    # H - (k, n) NumPy array, where each entry H[j][i] is the probability that X[i] is labeled as j
+    #b = np.zeros((Y.size, H.shape[0]))
+    #b[np.arange(Y.size),Y] = 1
+    #Use sparse matrix instead 
+    k=H.shape[0]
+    n=X.shape[0]
+    M = sparse.coo_matrix(([1]*n, (Y, range(n))), shape=(k,n)).toarray()
+    
+    # M -(k,n)
+    for i in range(k):
+
+        theta[i]=theta[i]-(alpha*(-np.sum(np.multiply(X,(M[i]-H[i])[:, None]),axis=0)/(X.shape[0]* temp_parameter)+lambda_factor * theta[i]))
+    return theta
+        
+    
+    
+
+    
+    
+
+def update_y(train_y, test_y):
+    """
+    Changes the old digit labels for the training and test set for the new (mod 3)
+    labels.
+
+    Args:
+        train_y - (n, ) NumPy array containing the labels (a number between 0-9)
+                 for each datapoint in the training set
+        test_y - (n, ) NumPy array containing the labels (a number between 0-9)
+                for each datapoint in the test set
+
+    Returns:
+        train_y_mod3 - (n, ) NumPy array containing the new labels (a number between 0-2)
+                     for each datapoint in the training set
+        test_y_mod3 - (n, ) NumPy array containing the new labels (a number between 0-2)
+                    for each datapoint in the test set
+    """
+    train_y_mod3= train_y%3
+    test_y_mod3 = test_y%3
+    return (train_y_mod3,test_y_mod3)
+    
+def compute_test_error_mod3(X, Y, theta, temp_parameter):
+    """
+    Returns the error of these new labels when the classifier predicts the digit. (mod 3)
+
+    Args:
+        X - (n, d - 1) NumPy array (n datapoints each with d - 1 features)
+        Y - (n, ) NumPy array containing the labels (a number from 0-2) for each
+            data point
+        theta - (k, d) NumPy array, where row j represents the parameters of our
+                model for label j
+        temp_parameter - the temperature parameter of softmax function (scalar)
+
+    Returns:
+        test_error - the error rate of the classifier (scalar)
+    """
+    error_count = 0.
+    assigned_labels = get_classification(X, theta, temp_parameter)
+    assigned_labels%=3
+    return 1 - np.mean(assigned_labels == Y)
+
+def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterations):
+    """
+    Runs batch gradient descent for a specified number of iterations on a dataset
+    with theta initialized to the all-zeros array. Here, theta is a k by d NumPy array
+    where row j represents the parameters of our model for label j for
+    j = 0, 1, ..., k-1
+
+    Args:
+        X - (n, d - 1) NumPy array (n data points, each with d-1 features)
+        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
+            data point
+        temp_parameter - the temperature parameter of softmax function (scalar)
+        alpha - the learning rate (scalar)
+        lambda_factor - the regularization constant (scalar)
+        k - the number of labels (scalar)
+        num_iterations - the number of iterations to run gradient descent (scalar)
+
+    Returns:
+        theta - (k, d) NumPy array that is the final value of parameters theta
+        cost_function_progression - a Python list containing the cost calculated at each step of gradient descent
+    """
+    X = augment_feature_vector(X)
+    theta = np.zeros([k, X.shape[1]])
+    cost_function_progression = []
+    for i in range(num_iterations):
+        cost_function_progression.append(compute_cost_function(X, Y, theta, lambda_factor, temp_parameter))
+        theta = run_gradient_descent_iteration(X, Y, theta, alpha, lambda_factor, temp_parameter)
+    return theta, cost_function_progression
+
+def get_classification(X, theta, temp_parameter):
+    """
+    Makes predictions by classifying a given dataset
+
+    Args:
+        X - (n, d - 1) NumPy array (n data points, each with d - 1 features)
+        theta - (k, d) NumPy array where row j represents the parameters of our model for
+                label j
+        temp_parameter - the temperature parameter of softmax function (scalar)
+
+    Returns:
+        Y - (n, ) NumPy array, containing the predicted label (a number between 0-9) for
+            each data point
+    """
+    X = augment_feature_vector(X)
+    probabilities = compute_probabilities(X, theta, temp_parameter)
+    return np.argmax(probabilities, axis = 0)
+
+def plot_cost_function_over_time(cost_function_history):
+    plt.plot(range(len(cost_function_history)), cost_function_history)
+    plt.ylabel('Cost Function')
+    plt.xlabel('Iteration number')
+    plt.show()
+
+def compute_test_error(X, Y, theta, temp_parameter):
+    error_count = 0.
+    assigned_labels = get_classification(X, theta, temp_parameter)
+    return 1 - np.mean(assigned_labels == Y)
